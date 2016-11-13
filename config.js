3118b0fed6ba894db70d4e143454d85852c12c8d
chipsetworldwide <shane@chipsetworldwide.com>
2016-11-13 21:08:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAYMF4DowxoBAIDhPCCfLGsieON9Vp1E4qlm0DRvrDnVPQNKa0GR7FDInwCCbthwMY1yKSIdk050baRIvbmVe8mG2bDAdwzdMa58avZCy9Ixp5Nh54Eog8dopquMZB8NgZAdfW01w2B8vG75t3oVaBg5ZBn6LKWdfevOP2q3nQZDZD',
+    FB_VERIFY_TOKEN: 'my_company_is_chipsetworldwide',
+    API_AI_CLIENT_ACCESS_TOKEN: 'e7fb32891fc3461fa599ba27a9c27b39',
+    FB_APP_SECRET: '3197435918f66cab23aa44008cd921e0',
+    SERVER_URL: "https://chipsetworldwide-bot-2.herokuapp.com/",
 };
